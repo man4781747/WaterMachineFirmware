d60ec60257a11b69ecdc8f01dd9dd629fe2806ce
Akira Chen <man4781747@gmail.com>
2023-08-08 14:30:26 +0800

    ExFile_listDir 現在會輸出檔案名稱、大小、最後編輯時間

--- lib/StorgeSystemExternalFunction/StorgeSystemExternalFunction.h
+++ lib/StorgeSystemExternalFunction/StorgeSystemExternalFunction.h
@@ -5,8 +5,8 @@
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
 
-inline DynamicJsonDocument ExFile_listDir(fs::FS& fileSystem, String dir) {
-  DynamicJsonDocument fileNameListItem(1000);
+DynamicJsonDocument ExFile_listDir(fs::FS& fileSystem, String dir) {
+  DynamicJsonDocument fileNameListItem(10000);
   if (fileSystem.exists(dir)) {
     File folder = fileSystem.open(dir.c_str());
     while (true) {
@@ -14,8 +14,17 @@ inline DynamicJsonDocument ExFile_listDir(fs::FS& fileSystem, String dir) {
       if (! Entry) {
         break;
       }
-      fileNameListItem.add(String(Entry.name()));
+      DynamicJsonDocument fileInfo(500);
+      fileInfo["size"].set(Entry.size());
+      fileInfo["name"].set(String(Entry.name()));
+      fileInfo["getLastWrite"].set(Entry.getLastWrite());
+      // Serial.println("===============");
+      // Serial.println(Entry.name());
+      // Serial.println(Entry.getLastWrite());
+      // Serial.println(Entry.size());
+      // fileNameListItem.add(String(Entry.name()));
       Entry.close();
+      fileNameListItem.add(fileInfo);
     }
   }
   return fileNameListItem;
