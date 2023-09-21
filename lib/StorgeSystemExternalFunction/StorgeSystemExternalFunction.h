7533989d76f4fb7a8687b0748f95adf92fd46b84
man4781747 <man4781747@gmail.com>
2023-09-21 14:17:03 +0800

    Update StorgeSystemExternalFunction.h

--- lib/StorgeSystemExternalFunction/StorgeSystemExternalFunction.h
+++ lib/StorgeSystemExternalFunction/StorgeSystemExternalFunction.h
@@ -1,11 +1,11 @@
-#ifndef ExFile_FUNCTION_H
-#define ExFile_FUNCTION_H
+#ifndef EXFILE_FUNCTION_H
+#define EXFILE_FUNCTION_H
 #include <esp_log.h>
 #include <SD.h>
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
 
-DynamicJsonDocument ExFile_listDir(fs::FS& fileSystem, String dir) {
+inline DynamicJsonDocument ExFile_listDir(fs::FS& fileSystem, String dir) {
   DynamicJsonDocument fileNameListItem(10000);
   if (fileSystem.exists(dir)) {
     File folder = fileSystem.open(dir.c_str());
@@ -28,6 +28,61 @@ DynamicJsonDocument ExFile_listDir(fs::FS& fileSystem, String dir) {
     }
   }
   return fileNameListItem;
+}
+
+inline bool ExFile_CreateFile(fs::FS& fileSystem, String FilePath) {
+  ESP_LOGI("", "建立檔案: %s", FilePath.c_str());
+  String folderPath = FilePath.substring(0,FilePath.lastIndexOf('/')+1);
+  if (folderPath.indexOf('/') == 0){
+    folderPath = folderPath.substring(1,folderPath.length());
+  }
+  String folderPathStack = "";
+  int index;
+  int length;
+  while (1){
+    index = folderPath.indexOf('/');
+    if (index <0) {
+      break;
+    }
+    folderPathStack = folderPathStack + "/" + folderPath.substring(0,index);
+    folderPath = folderPath.substring(index+1,folderPath.length());
+    if (fileSystem.exists(folderPathStack) == false){
+      ESP_LOGD("", "建立資料夾: %s", folderPathStack.c_str());
+      if (fileSystem.mkdir(folderPathStack) == false) {
+        return false;
+      }
+    }
+  }
+  if (fileSystem.exists(FilePath) == false){
+    File selectedFile = fileSystem.open(FilePath, FILE_WRITE);
+    selectedFile.close();
+  }
+  return true;
+}
+
+inline bool ExFile_WriteJsonFile(fs::FS& fileSystem, String filePath, JsonDocument &jsonData) {
+  if (!fileSystem.exists(filePath)) {
+    ExFile_CreateFile(fileSystem, filePath);
+  }
+  File file = fileSystem.open(filePath, FILE_WRITE);
+  size_t writeSize = serializeJson(jsonData, file);
+  file.close();
+  ESP_LOGD("", "已將JSON格式資料寫入檔案: %s, 寫入長度: %d", filePath.c_str(), writeSize);
+  return true;
+}
+
+inline bool ExFile_LoadJsonFile(fs::FS& fileSystem, String filePath, JsonDocument &jsonData) {
+  if (fileSystem.exists(filePath)) {
+    File file = fileSystem.open(filePath, FILE_READ);
+    DeserializationError error = deserializeJson(jsonData, file);
+    if (error) {
+      ESP_LOGE("", "JSON格式檔案: %s 讀取失敗，失敗原因: %s", filePath.c_str(), error.c_str());
+      return false;
+    }
+    return true;
+  }
+  ESP_LOGE("", "找不到檔案: %s, 無法讀取", filePath.c_str());
+  return false;
 }
 
 #endif
