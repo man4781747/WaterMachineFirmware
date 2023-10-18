7de8512cce813b12a2d15facf6952897cfd293e1
man4781747 <man4781747@gmail.com>
2023-10-18 09:34:10 +0800

    刪除不動要的部分

--- src/main.cpp
+++ src/main.cpp
@@ -39,9 +39,7 @@
 // DFRobot_MCP9808_I2C mcp9808(&Machine_Ctrl.WireOne, 0x18);
 // DFRobot_MCP9808_I2C mcp9808_other(&Machine_Ctrl.WireOne, 0x19);
 
-void testWeb(int index, int type, String desp);
-
-const char* FIRMWARE_VERSION = "V3.23.94.0";
+const char* FIRMWARE_VERSION = "V3.23.1003.0";
 
 void scanI2C();
 
@@ -97,62 +95,7 @@ void setup() {
 }
 
 void loop() {
-
-  // test = Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.TakeOneValue();
-  // Serial.printf("%s, %d,%d,%.2f\n", Machine_Ctrl.GetDatetimeString().c_str(), test.CH_0, test.CH_1,mcp9808_other.getTemperature());
-  // uint16_t CH_Buff [30];
-  // uint16_t Temp_Buff[30];
-  // for (int i=0;i<30;i++) {
-  //   test = Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.TakeOneValue();
-  //   CH_Buff[i] = test.CH_0;
-  //   // Temp_Buff[i] = (uint16_t)(mcp9808_other.getTemperature());
-  // }
-
-  // Serial.printf("%s, %.2f,%.2f\n", 
-  //   Machine_Ctrl.GetDatetimeString().c_str(), 
-  //   afterFilterValue(CH_Buff,30), 
-  //   mcp9808_other.getTemperature()
-  //   // afterFilterValue(Temp_Buff,30)
-  // );
-
-  // for (int i=151;i<156;i=i+1) {
-  //   Machine_Ctrl.WireOne.beginTransmission(0x2F);
-  //   Machine_Ctrl.WireOne.write(0b00000000);
-  //   Machine_Ctrl.WireOne.write(i);
-  //   Machine_Ctrl.WireOne.endTransmission();
-  //   Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.SetGain(ALS_Gain::Gain_48X);
-  //   test = Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.TakeOneValue();
-  //   Serial.printf("%s, %d,%d,%d,%.2f\n", Machine_Ctrl.GetDatetimeString().c_str(), i, test.CH_0, test.CH_1,mcp9808_other.getTemperature());
-    // Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.SetGain(ALS_Gain::Gain_96X);
-    // test = Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.TakeOneValue();
-
-    // Serial.printf("%d,%d,%.2f\n", test.CH_0, test.CH_1,mcp9808_other.getTemperature());
-    // Serial.printf("%d,%d,%.2f\n", test.CH_0, test.CH_1,mcp9808_other.getTemperature());
-    // mcp9808_other.wakeUpMode();
-    // mcp9808_other.setResolution(RESOLUTION_0_0625);
-
-  // }
-  // Machine_Ctrl.GetDatetimeString();
-  // testWeb(0, 148, "綠光測試: 148");
-  // testWeb(1, 248, "藍光測試: 248");
-  // for (int indexChose = 0;indexChose<2;indexChose++) {
-    // for (int typeChose = 0;typeChose<=200;typeChose=typeChose+10) {
-      // testWeb(indexChose, 160);
-    // }
-  // }
-  
-  // digitalWrite(48, HIGH);
-  // delay(500);
-  // Machine_Ctrl.peristalticMotorsCtrl.SetMotorStatus(0, PeristalticMotorStatus::FORWARD);
-  // Machine_Ctrl.peristalticMotorsCtrl.RunMotor(
-  //   Machine_Ctrl.peristalticMotorsCtrl.moduleDataList
-  // );
-  // delay(1500);
-  // Machine_Ctrl.peristalticMotorsCtrl.SetAllMotorStop();
-  // digitalWrite(48, LOW);
-  // scanI2C();
-  // Machine_Ctrl.ShowIPAndQRCodeOnOled();
-  delay(2000);
+  vTaskDelay(9999999/portTICK_PERIOD_MS);
 }
 
 void scanI2C(){
@@ -174,61 +117,4 @@ void scanI2C(){
   if (devices == 0) {
     Serial.println("No I2C devices found");
   }
-}
-
-void testWeb(int index, int type, String desp) {
-  HTTPClient http;
-  DynamicJsonDocument PostData_V2(10000);
-  uint16_t CH0_Buff_V2 [30];
-  uint16_t CH1_Buff_V2 [30];
-  uint16_t Temp_Buff[30];
-
-  Machine_Ctrl.WireOne.beginTransmission(0x70);
-  Machine_Ctrl.WireOne.write(1 << index);
-  Machine_Ctrl.WireOne.endTransmission();
-  Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.openSensorByIndex(index);
-  delay(100);
-  Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.SetGain(ALS_Gain::Gain_96X);
-  Machine_Ctrl.WireOne.beginTransmission(0x2F);
-  Machine_Ctrl.WireOne.write(0b00000000);
-  Machine_Ctrl.WireOne.write(type);
-  Machine_Ctrl.WireOne.endTransmission();
-  for (int i=0;i<30;i++) {
-    ALS_01_Data_t test = Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.TakeOneValue();
-    CH0_Buff_V2[i] = test.CH_0;
-    CH1_Buff_V2[i] = test.CH_1;
-  }
-  PostData_V2["CH0"].set(afterFilterValue(CH0_Buff_V2,30));
-  PostData_V2["CH1"].set(afterFilterValue(CH1_Buff_V2,30));
-  //TODO 溫度測試
-  DFRobot_MCP9808_I2C mcp9808(&Machine_Ctrl.WireOne, 0x18);
-  DFRobot_MCP9808_I2C mcp9808_other(&Machine_Ctrl.WireOne, 0x19);
-
-
-
-  mcp9808.wakeUpMode();
-  mcp9808.setResolution(RESOLUTION_0_0625);
-  mcp9808_other.wakeUpMode();
-  mcp9808_other.setResolution(RESOLUTION_0_0625);
-  PostData_V2["Temperature"].set(mcp9808.getTemperature()) ;
-  PostData_V2["Temperature_2"].set(mcp9808_other.getTemperature()) ;
-  //TODO 溫度測試
-  PostData_V2["Type"].set("X96");
-  PostData_V2["SensorIndex"].set(index);
-  PostData_V2["Name"].set(desp);
-  PostData_V2["Resistance_Level"].set(type);
-  time_t nowTime = now();
-  char datetimeChar[30];
-  sprintf(datetimeChar, "%04d-%02d-%02d %02d:%02d:%02d",
-    year(nowTime), month(nowTime), day(nowTime),
-    hour(nowTime), minute(nowTime), second(nowTime)
-  );
-  PostData_V2["DataTime"] = datetimeChar;
-  String PostString = "";
-  http.begin("http://192.168.20.27:5566/data");
-  http.addHeader("Content-Type", "application/json");
-  serializeJsonPretty(PostData_V2, PostString);
-  http.POST(PostString);
-  http.end();
-  Machine_Ctrl.MULTI_LTR_329ALS_01_Ctrler.closeAllSensor();
 }
